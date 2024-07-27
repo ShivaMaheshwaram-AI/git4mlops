741b08f6339677fc2714e66946dfbc7bc31ed2dd
shivamaheshwaram <shivamaheshwaram1995@gmail.com>
2024-07-27 15:05:34 +0530

    my third commit

--- file1.py
+++ file1.py
@@ -1,2 +1,12 @@
 print(f"hello my frinds this output is from file1.py")
 print(f"this is the second line")
+
+'''
+git init
+git status
+git add file1.py
+git status
+git commit -m "message for the commit"
+git status
+git log
+'''
