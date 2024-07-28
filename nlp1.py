8588f8f4f72045dfeeed38e802d270dc2587a7db
shivamaheshwaram <shivamaheshwaram1995@gmail.com>
2024-07-28 15:58:49 +0530

    commit 6 nlp1

---
+++ nlp1.py
@@ -0,0 +1,8 @@
+library = [
+    ('athor', "Topic", 'pages'),
+    ('shiva', 'ml for education', 5),
+    ('shiva', 'optimization in investing', '10')
+]
+# print(library)
+for book in library :
+    print (book)
